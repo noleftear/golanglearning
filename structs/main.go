b947124d1cf579bee1bd0459fc8deb18e4db3a52
jhurst <jhurst@samaritan.org>
2024-01-08 10:54:40 -0500

    Structs with receiver functions

--- structs/main.go
+++ structs/main.go
@@ -20,7 +20,10 @@ func main() {
 	jeff.lastName = "Hurst"
 	jeff.info = personInfo{age: 31, weight: 180, gender: "male"}
 	josh := person{firstName: "Joshua", lastName: "Hurst", info: personInfo{age: 29, weight: 225, gender: "male"}}
-	fmt.Println(josh.firstName, josh.lastName, "is a", josh.info.age, "year old", josh.info.gender, "that weighs", josh.info.weight, "pounds.")
-	fmt.Println(jeff.firstName, jeff.lastName, "is a", jeff.info.age, "year old", jeff.info.gender, "that weighs", jeff.info.weight, "pounds.")
+	josh.printPerson()
+	jeff.printPerson()
+}
 
+func (p person) printPerson() {
+	fmt.Println(p.firstName, p.lastName, "is a", p.info.age, "year old", p.info.gender, "that weighs", p.info.weight, "pounds.")
 }
